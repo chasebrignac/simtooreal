030b5bbbf4a1ebc88563670957b7c04422fae823
Chase J Brignac <chase@viable.fit>
2022-03-26 21:04:32 -0700

    providers should be optional

--- variables.tf
+++ variables.tf
@@ -8,12 +8,6 @@ terraform {
   }
 }
 
-# This is to avoid terraform complaining about no region specified
-provider "aws" {
-  profile = "default"
-  region = "us-east-1"
-}
-
 # The major region is us-east-1
 variable "aws_region" {
   description = "The AWS region"
@@ -24,12 +18,6 @@ variable "aws_region" {
 variable "az_count" {
   description = "Number of AZs to cover in a given AWS region"
   default     = "2"
-}
-
-# The number of app containers to normally run
-variable "app_count" {
-  description = "Number of app containers to run"
-  default     = 1
 }
 
 # The database password should be passed in at terraform runtime by using -var
@@ -51,27 +39,4 @@ variable "aws_access_key_id" {
   description = "AWS key ID"
   default     = "$$${aws_access_key_id}"
   sensitive   = true
-}
-
-# The database password should be passed in at terraform runtime by using -var
-variable "openai_api_key" {
-  description = "OpenAI API Key"
-  default     = "$$${openai_api_key}"
-  sensitive   = true
-}
-
-# The minimum number of containers that should be running
-variable "ecs_autoscale_min_instances" {
-  default = "2"
-}
-
-# The maximum number of containers that should be running
-variable "ecs_autoscale_max_instances" {
-  default = "5"
-}
-
-# The number of instances to reserve
-variable "reserved_instances_count" {
-  description = "Number of reserved instances to run"
-  default     = 1
 }
